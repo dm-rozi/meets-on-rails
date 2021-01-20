8675d1474766f1166e70d399f1a8160a71141c86
dm-rozi <dmitry.rozi@gmail.com>
2021-01-21 01:43:48 +0300

    add ruby 2.6.6

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+ruby '2.6.6'
+
 gem 'rails', '~> 6.1.0'
 gem 'sqlite3', '~> 1.4'
 gem 'puma', '~> 5.0'
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module MeetsOnRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ru
+    config.i18n.fallbacks = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
